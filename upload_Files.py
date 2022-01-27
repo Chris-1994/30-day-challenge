ad997d6de88fba1555ac9075feec5434260cead5
Chris Jadama <Chrisjadama@gmail.com>
2022-01-27 06:09:12 +0100

    U

--- upload_Files.py
+++ upload_Files.py
@@ -4,4 +4,4 @@ token = "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"
 
 subprocess.call(['git', 'add', '-A'])
 subprocess.call(['git', 'commit', '-m', '{}'.format("U")])
-subprocess.call(['git', 'push', 'https://{}@github.com/user-name/repo.git'.format(token)])
+subprocess.call(['git', 'push', 'https://{}@github.com/Chris-1994/30-day-challenge'.format(token)])
