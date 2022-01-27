4383af1e41b1df1d89938d87490249dcc3ccfb8f
Chris Jadama <Chrisjadama@gmail.com>
2022-01-27 06:11:04 +0100

    U

--- upload_Files.py
+++ upload_Files.py
@@ -1,6 +1,6 @@
 import subprocess
 
-token = ""
+token = "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"
 
 subprocess.call(['git', 'add', '-A'])
 subprocess.call(['git', 'commit', '-m', '{}'.format("U")])
