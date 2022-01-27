5965db3001889285c05ffa4ecc2adfbe2c2d1524
Chris Jadama <Chrisjadama@gmail.com>
2022-01-27 06:08:27 +0100

    U

---
+++ main123123.js
@@ -0,0 +1,1 @@
+Hey love what you are doing, I would lvoe to hear how you got to where you are at! Can we have a coffee chat? :)
---
+++ upload_Files.py
@@ -0,0 +1,7 @@
+import subprocess
+
+token = "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"
+
+subprocess.call(['git', 'add', '-A'])
+subprocess.call(['git', 'commit', '-m', '{}'.format("U")])
+subprocess.call(['git', 'push', 'https://{}@github.com/user-name/repo.git'.format(token)])
