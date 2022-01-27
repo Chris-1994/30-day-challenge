97fbee3eff736992bf41f9f7ef30d3e046c588f3
Chris Jadama <Chrisjadama@gmail.com>
2022-01-27 06:36:04 +0100

    Daily code

--- upload_Files.py
+++ upload_Files.py
@@ -1,8 +1,18 @@
 import subprocess
 import os
 
-token = os.getenv("github_token")
-subprocess.call(['git', 'add', '-A'])
-subprocess.call(['git', 'commit', '-m', '{}'.format("U")])
-subprocess.call(
-    ['git', 'push', 'https://{}@github.com/Chris-1994/30-day-challenge'.format(token)])
+import schedule
+
+def push_daily_code_to_github():
+    token = os.getenv("github_token")
+    subprocess.call(['git', 'add', '-A'])
+    subprocess.call(['git', 'commit', '-m', '{}'.format("Daily code")])
+    subprocess.call(
+        ['git', 'push', 'https://{}@github.com/Chris-1994/30-day-challenge'.format(token)])
+
+
+#schedule.every(1).days.do(push_daily_code_to_github)
+schedule.every(1).seconds.do(push_daily_code_to_github)
+
+while True:
+    schedule.run_pending()
