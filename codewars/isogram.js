56a7f30c9ad01f6de7daed5881742abcbedc9b69
Chris Jadama <Chrisjadama@gmail.com>
2022-02-07 15:42:18 +0100

    u

--- codewars/isogram.js
+++ codewars/isogram.js
@@ -1,3 +1,6 @@
 function isIsogram(str) {
   return !str.match(/([a-z]).*\1/i);
 }
+
+
+<p><a href="https://www.sjekkregnskapet.no/products/aksjonaerregisteroppgaven-uten-stress/categories/4258098/posts/14296158?wvideo=7t7qm6ukqe"><img src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production//site/119539/products/6adf9426-8903-4d3a-a446-59d6de508adf.jpg" width="400" height="225" style="width: 400px; height: 225px;"></a></p><p><a href="https://www.sjekkregnskapet.no/products/aksjonaerregisteroppgaven-uten-stress/categories/4258098/posts/14296158?wvideo=7t7qm6ukqe">Sjekk Regnskapet</a></p>
