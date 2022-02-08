a013130a70d15ded3b9b1498ae0a4cd48c52442a
Chris Jadama <Chrisjadama@gmail.com>
2022-02-08 17:21:34 +0100

    u

---
+++ class32-materials/methodWork.js
@@ -0,0 +1,34 @@
+// what is charAt
+
+const stringW = "WORDS ";
+console.log(stringW.charAt(2));
+
+// what is charCodeAt
+console.log(stringW.charCodeAt(2));
+// what is concat
+const l = [1, 2, 3];
+const d = [1, 24, 3];
+console.log(d.concat(l));
+
+// what is includes
+console.log(l.includes(2));
+// what is indexOf
+console.log(l.indexOf(2));
+// what is match
+console.log(stringW.match(0 - 9));
+// what is repeat
+console.log(stringW.repeat(2));
+// what is search
+console.log(stringW.search("W"));
+// what is slice
+
+const p = [1, 24, 3].slice(0, 2);
+console.log(p);
+// what is substr
+console.log(stringW.substr(1, 2));
+// what is toLowerCase
+console.log(stringW.toLowerCase());
+// what is trim
+console.log(stringW.trim(1));
+// what is toUpperCase
+console.log(stringW.toUpperCase());
